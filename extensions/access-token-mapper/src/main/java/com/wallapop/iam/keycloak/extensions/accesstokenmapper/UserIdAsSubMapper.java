e622f2b1057bf802428ad4d0d9346ece4be54490
Romina Angeloni <84572550+romiangeloniwallapop@users.noreply.github.com>
2024-05-30 12:35:46 +0200

    [WPA-57522] Configure protocol mapper to add userId as a claim on token (#21)

---
+++ extensions/access-token-mapper/src/main/java/com/wallapop/iam/keycloak/extensions/accesstokenmapper/UserIdAsSubMapper.java
@@ -0,0 +1,59 @@
+package com.wallapop.iam.keycloak.extensions.accesstokenmapper;
+
+import org.keycloak.models.*;
+import org.keycloak.protocol.oidc.mappers.*;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.representations.IDToken;
+import org.keycloak.storage.StorageId;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserIdAsSubMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper,
+        OIDCIDTokenMapper, UserInfoTokenMapper {
+
+    public static final String PROVIDER_ID = "user-id-as-sub-mapper";
+
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
+    static {
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, UserIdAsSubMapper.class);
+    }
+
+    @Override
+    public String getDisplayCategory() {
+        return "Wallapop User ID Token Mapper";
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Overwrite Sub with Wallapop User ID";
+    }
+
+    @Override
+    public String getHelpText() {
+        return "User ID from Wallapop";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    protected void setClaim(
+            IDToken token,
+            ProtocolMapperModel mappingModel,
+            UserSessionModel userSession,
+            KeycloakSession keycloakSession,
+            ClientSessionContext clientSessionCtx
+    ) {
+        String wallapopUserId = StorageId.externalId(token.getSubject());
+        token.setSubject(wallapopUserId);
+    }
+}
