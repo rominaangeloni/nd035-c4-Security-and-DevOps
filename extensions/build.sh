5f0ef0a6027b9086ec0b48e2aedc58cd9aa57a87
Alejandro Garcia Seco <agseco@users.noreply.github.com>
2024-07-02 09:37:51 +0200

    WPA-53510 - Keycloak HA (#58)

--- extensions/build.sh
+++ extensions/build.sh
@@ -3,5 +3,5 @@
 echo "Building extensions"
 
 ./mvnw -f access-token-mapper/pom.xml clean install
-
 ./mvnw -f monolith-users/pom.xml clean install
+./mvnw -f jgroups-aws-bundle/pom.xml clean install
