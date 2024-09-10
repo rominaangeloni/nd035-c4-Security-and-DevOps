dcf7af5b72617b343aa8f81ba65e449506235b22
carloswalla <97446026+carloswalla@users.noreply.github.com>
2024-09-10 12:18:22 +0200

    Wpa 64167 avoid 169.x.x.x IPs (#111)
    Co-authored-by: davidpsv17 <david.lopez@wallapop.com>

---
+++ infrastructure/docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# There is a bug on how jgroups auto-discovers ECS instance nodes, we'll make use of this new env var rather than
+# default jgroups ec2 parameters
+export ECS_INSTANCE_PRIVATE_ADDRESS=$(curl ${ECS_CONTAINER_METADATA_URI} | jq -r .Networks[0].IPv4Addresses[0])
+
+# This will exec the CMD from your Dockerfile
+exec "$@"
