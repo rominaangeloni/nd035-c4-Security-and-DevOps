70f07909edaf917fa03c8fd6b4135d60b24f557a
carloswalla <97446026+carloswalla@users.noreply.github.com>
2024-09-12 11:52:32 +0200

    Force new deploy to recover primary property from AWS Service's Parameter Store (#113)

--- infrastructure/docker/entrypoint.sh
+++ infrastructure/docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# There is a bug on how jgroups auto-discovers ECS instance nodes, we'll make use of this new env var rather than
-# default jgroups ec2 parameters
+# We've detected a bug (not published yet) on how jgroups auto-discovers ECS instance nodes, we'll make use of this
+# new env var rather than default jgroups ec2 parameters
 export ECS_INSTANCE_PRIVATE_ADDRESS=$(curl ${ECS_CONTAINER_METADATA_URI} | jq -r .Networks[0].IPv4Addresses[0])
 
 # This will exec the CMD from your Dockerfile
